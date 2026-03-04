4896a7d4d77cac5865a0eb7a567ef793b111567c
Natural-Minecraft <naturalsmp.mc@gmail.com>
2026-03-05 04:41:49 +0700

    feat: implement IridiumColorAPI and brand new NaturalSMP UX overhaul

--- src/main/java/id/naturalsmp/naturalbounties/features/LanguageOptions.java
+++ src/main/java/id/naturalsmp/naturalbounties/features/LanguageOptions.java
@@ -38,6 +38,7 @@ import java.io.InputStreamReader;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import com.iridium.iridiumcolorapi.IridiumColorAPI;
 
 
 import static me.naturalsmp.NaturalBounties.features.settings.integrations.external_api.LocalTime.formatTime;
@@ -555,36 +556,8 @@ public class LanguageOptions {
     }
 
     public static String color(String str){
-        str = net.md_5.bungee.api.ChatColor.translateAlternateColorCodes('&', str);
-        return cancelColorCodes("</#", ">", translateHexColorCodes("<#", ">", translateHexColorCodes("&#","", str)));
-    }
-    public static String translateHexColorCodes(String startTag, String endTag, String message)
-    {
-        final Pattern hexPattern = Pattern.compile(startTag + "([A-Fa-f0-9]{6})" + endTag);
-        Matcher matcher = hexPattern.matcher(message);
-        StringBuilder buffer = new StringBuilder(message.length() + 4 * 8);
-        while (matcher.find())
-        {
-            String group = matcher.group(1);
-            matcher.appendReplacement(buffer, COLOR_CHAR + "x"
-                    + COLOR_CHAR + group.charAt(0) + COLOR_CHAR + group.charAt(1)
-                    + COLOR_CHAR + group.charAt(2) + COLOR_CHAR + group.charAt(3)
-                    + COLOR_CHAR + group.charAt(4) + COLOR_CHAR + group.charAt(5)
-            );
-        }
-        return matcher.appendTail(buffer).toString();
-    }
-    public static String cancelColorCodes(String startTag, String endTag, String message)
-    {
-        final Pattern hexPattern = Pattern.compile(startTag + "([A-Fa-f0-9]{6})" + endTag);
-        Matcher matcher = hexPattern.matcher(message);
-        StringBuilder buffer = new StringBuilder(message.length() + 4 * 8);
-        while (matcher.find())
-        {
-            matcher.appendReplacement(buffer, org.bukkit.ChatColor.RESET + ""
-            );
-        }
-        return matcher.appendTail(buffer).toString();
+        if (str == null) return null;
+        return IridiumColorAPI.process(str);
     }
 
 
